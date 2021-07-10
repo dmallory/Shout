bd0d264b6efb3ee9a64c8cf5d246edd83cca70f2
Dustin Mallory <dustin.mallory@gmail.com>
2021-07-10 00:00:01 -0600

    Use ios ssh libs

--- Sources/CSSH/shim.h
+++ Sources/CSSH/shim.h
@@ -1,8 +1,8 @@
 #ifndef __CLIBSSH_SHIM_H__
 #define __CLIBSSH_SHIM_H__
 
-#include <libssh2.h>
-#include <libssh2_sftp.h>
-#include <libssh2_publickey.h>
+#include "/Users/dustin/code/chia/iSSH2/libssh2_iphoneos/include/libssh2.h"
+#include "/Users/dustin/code/chia/iSSH2/libssh2_iphoneos/include/libssh2_sftp.h"
+#include "/Users/dustin/code/chia/iSSH2/libssh2_iphoneos/include/libssh2_publickey.h"
 
 #endif
